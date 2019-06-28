ea005a409d3946ff6070795bbb2a56b8ac5b36a0
Vishal Grover <grovervishal591@gmail.com>
2019-06-28 11:53:15 -0400

    first 7 videos

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,7 @@
+const Home = props =>{
+    return <div>
+            <h1>This is index page</h1>
+        </div>
+};
+
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,9 @@
+const Sell = props =>{
+    return (
+    <div>
+        <h1>Selling it</h1>
+    </div>
+    );
+};
+
+export default Sell;
